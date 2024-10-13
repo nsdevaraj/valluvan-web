7f4e6cbb88b97982d852490cc4f39b4ad70771f4
Devaraj <nsdevaraj@gmail.com>
2024-10-13 11:08:50 +0530

    fix text

--- src/utils/TranslationUtil.js
+++ src/utils/TranslationUtil.js
@@ -1,7 +1,7 @@
 const titlesByLanguage = {
   Tamil: ["அறத்துப்பால்", "பொருட்பால்", "இன்பத்துப்பால்"],
   English: ["Virtue", "Wealth", "Nature of Love"],
-  telugu: ["ధ్మం", "సంపద", "్రేమ స్వభావం"],
+  telugu: ["ధర్మం", "సంపద", "ప్రేమ స్వభావం"],
   hindi: ["धर्म", "धन", "प्रेम"],
   kannad: ["ಧರ್ಮ", "సంపద", "ಪ್ರೇಮ"],
   french: ["Perfection", "Richesse", "Nature de l'Amour"],
@@ -16,6 +16,39 @@ const titlesByLanguage = {
   singalam: ["දානය", "අරමුණ", "සතුට"],
   swedish: ["Dygd", "Välst", "Kärlekens natur"],
 };
+
+export function getSystemImageForIyal(iyal) {
+  switch (iyal) {
+    case ("Preface", "பாயிரவியல்"):
+      return "book.fill";
+    case ("Domestic Virtue", "இல்லறவியல்"):
+      return "house.fill";
+    case ("Ascetic Virtue", "துறவறவியல்"):
+      return "leaf.fill";
+    case ("Royalty", "அரசியல்"):
+      return "crown.fill";
+    case ("Ministry", "அமைச்சியல்"):
+      return "briefcase.fill";
+    case ("Politics", "அரணியல்"):
+      return "building.columns.fill";
+    case ("Friendship", "நட்பியல்"):
+      return "person.2.fill";
+    case "Miscellaneous":
+      return "square.grid.2x2.fill";
+    case ("கூழியல்", "pulp"):
+      return "cup.and.saucer.fill";
+    case ("படையில்", "army"):
+      return "shield.fill";
+    case ("குடியியல்", "civility"):
+      return "person.3.fill";
+    case ("Pre-marital love", "களவியல்"):
+      return "heart.fill";
+    case ("Post-marital love", "கற்பியல்"):
+      return "heart.circle.fill";
+    default:
+      return "circle.fill";
+  }
+}
 
 export const getLanguageSpecificColumns = (language) => {
   switch (language) {
